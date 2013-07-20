1648a15efd56f2ae7e6eb8fa5471ebeb87e81d19
tpennock <taylor.pennock@unicity.com>
2013-07-20 11:23:28 -0600

    initial code commit;

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/* Copyright (C) YOOtheme GmbH, YOOtheme Proprietary Use License (http://www.yootheme.com/license) */
+
+(function($){
+
+	$(document).ready(function() {
+
+		// Tooltips
+	    $('body').tooltip({
+	      selector: "a[data-toggle=tooltip]"
+	    })
+		
+	});
+
+})(jQuery);
